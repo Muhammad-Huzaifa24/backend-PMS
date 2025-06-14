6ac6e4b192ef134833a4853550dbca5673105e76
Muhammad Huzaifa <huzaifakhawar100@gmail.com>
2025-06-14 22:26:51 +0500

    update cors issue

--- src/constants.js
+++ src/constants.js
@@ -13,7 +13,7 @@ const refreshCookieOptions = {
 };
 
 const corsOptions = {
-  origin: 'http://localhost:5173',
+  origin: ['http://localhost:5173', 'http://localhost:5174'],
   credentials: true,
   exposedHeaders: ['Authorization'],
   allowedHeaders: ['Content-Type', 'Authorization'],
