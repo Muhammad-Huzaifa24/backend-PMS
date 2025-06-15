c2feedcf7ac51c9fcbb5593cb0efac4bf7ba1b35
Muhammad Huzaifa <huzaifakhawar100@gmail.com>
2025-06-15 13:06:04 +0500

    update cors options

--- src/config/corsOptions.js
+++ src/config/corsOptions.js
@@ -1,11 +1,10 @@
 // config/corsOptions.js
 const corsOptions = {
   origin: [
-    'http://localhost:5173',
-    'http://localhost:5174',
-    'https://project-management-dashboard-ruddy.vercel.app'
+    '*'
   ],
   credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   exposedHeaders: ['Authorization'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 };
